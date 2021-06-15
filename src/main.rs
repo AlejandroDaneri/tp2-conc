ef965c261ac1e8119051b7f34814a8ac3c323d4d
mlafroce <mlafroce@gmail.com>
2021-06-15 18:12:59 -0300

    Refactor de Thesaurus
    Agrega un trait `Finder` para usar de plantilla con los otros buscadores.

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,13 @@
 extern crate reqwest;
 
-use reqwest::blocking;
+mod synonym;
+
 use std::env::args;
 
+use crate::synonym::Finder;
+use crate::synonym::thesaurus::Thesaurus;
+
 const ARGS_MIN_LEN: usize = 2;
-
-fn extract_thesaurus(text: &str) {
-    let mut search_l = 0;
-    let mut search_r = 0;
-
-    match text.find("Compare Synonyms") {
-        None => {}
-        Some(index) => search_l = index,
-    };
-
-    let (_left, right) = text.split_at(search_l);
-
-    match right.rfind("<span color=\"#1A1A1A\" font-size=\"16px\"") {
-        None => {}
-        Some(index) => search_r = index,
-    };
-
-    let (left2, _right2) = right.split_at(search_r);
-    let mut vec: Vec<&str> = left2.split("href=\"/browse/").collect();
-    vec.remove(0);
-
-    let mut res: Vec<&str> = Vec::new();
-
-    for elem in vec.iter() {
-        let mut search_l = 0;
-        let mut search_r = 0;
-
-        match elem.find('>') {
-            None => {}
-            Some(index) => search_l = index + 1,
-        };
-
-        let (_left, right) = elem.split_at(search_l);
-
-        match right.find('<') {
-            None => {}
-            Some(index) => search_r = index,
-        };
-
-        let (left2, _right2) = right.split_at(search_r);
-
-        res.push(left2)
-    }
-
-    println!("{:?}", res);
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let argv: Vec<String> = args().collect();
@@ -57,10 +15,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // println!("Usage: {:?} <input file>", argv[0]);
         println!("Usage: specify word to search as an argument");
     } else {
-        let request = blocking::get(format!("https://www.thesaurus.com/browse/{}", argv[1]))?;
-        let text = request.text()?;
-        let string = text.as_str();
-        extract_thesaurus(string);
+        let word = argv[1].clone();
+        let q = Thesaurus::new_query(word.as_str());
+        let synonyms = q.find_synonyms()?;
+        println!("Sinonimos: {:?}", synonyms);
     }
     Ok(())
 }
---
+++ src/synonym/thesaurus.rs
@@ -0,0 +1,60 @@
+use super::Finder;
+
+pub struct Thesaurus {
+    word: String
+}
+
+impl Thesaurus {
+    pub fn new_query(word: &str) -> Self {
+        Self{word: word.to_owned()}
+    }
+}
+
+impl Finder for Thesaurus {
+    fn url(&self) -> String {
+        format!("https://www.thesaurus.com/browse/{}", self.word.as_str())
+    }
+
+    fn parse_body(&self, body: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+        let mut search_l = 0;
+        let mut search_r = 0;
+
+        match body.find("Compare Synonyms") {
+            None => {}
+            Some(index) => search_l = index,
+        };
+
+        let (_left, right) = body.split_at(search_l);
+
+        match right.rfind("<span color=\"#1A1A1A\" font-size=\"16px\"") {
+            None => {}
+            Some(index) => search_r = index,
+        };
+
+        let (left2, _right2) = right.split_at(search_r);
+        let mut vec: Vec<&str> = left2.split("href=\"/browse/").collect();
+        vec.remove(0);
+
+        let mut res: Vec<String> = Vec::new();
+
+        for elem in vec.iter() {
+            let mut search_l = 0;
+            let mut search_r = 0;
+
+            match elem.find('>') {
+                None => {}
+                Some(index) => search_l = index + 1,
+            };
+
+            let (_left, right) = elem.split_at(search_l);
+            match right.find('<') {
+                None => {}
+                Some(index) => search_r = index,
+            };
+            let (left2, _right2) = right.split_at(search_r);
+            res.push(left2.to_owned())
+        }
+
+        Ok(res)
+    }
+}
