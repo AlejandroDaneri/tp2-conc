15019118b051967bfa8a45cc946acf6c1ff011fa
romina <casal.romina@gmail.com>
2021-06-16 01:38:16 -0300

    reemplazar log y log4rs

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,19 @@ use crate::synonym::Finder;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let log = logger::Logger::new(logger::Level::Debug);
-    log.debug(format!("Configure log"));
+    log.debug("Configure log".to_string());
 
     let args: Vec<String> = env::args().collect();
 
     if args.len() <= 1 {
         println!("Missing path arg");
-        log.error(format!("Missing path arg"));
+        log.error("Missing path arg".to_string());
         return Ok(())
     }
 
     let path= args[1].as_str();
 
-    log.debug(format!("Opening file"));
+    log.debug("Opening file".to_string());
     let f = match File::open(path) {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {:?}", error),
@@ -34,7 +34,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let buffered = BufReader::new(f);
 
-    log.debug(format!("Reading file"));
+    log.debug("Reading file".to_string());
     for line in buffered.lines() {
         let word = line.unwrap();
         log.debug(format!("Searching synonyms for {}", word));
@@ -51,7 +51,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     }
 
-    log.debug(format!("Finish"));
+    log.debug("Finish".to_string());
 
     Ok(())
 
