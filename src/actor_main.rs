d8f42042134eb82cf3793456b08a897f67eb4a7f
Alejandro Daneri <adaneri@fi.uba.ar>
2021-06-27 15:50:57 -0300

    await una vez que terminaron todas las promesas

--- src/actor_main.rs
+++ src/actor_main.rs
@@ -51,6 +51,7 @@ async fn main() {
     let buffered = BufReader::new(f);
 
     log.debug("Reading file".to_string());
+    let mut promises = Vec::new();
     for line in buffered.lines() {
         let word = line.unwrap();
         log.debug(format!("Searching synonyms for {}", word));
@@ -59,12 +60,16 @@ async fn main() {
         let message = WordMessage {
             word: word.to_owned(),
         };
-        let res = match addr.send(message).await {
+        promises.push(addr.send(message))
+    }
+
+    for promise in promises {
+        let response = match promise.await {
             Ok(Ok(counter)) => counter,
             Err(_) => todo!(),
             Ok(Err(_)) => todo!(),
         };
-        println!("RESULT: {:?}", res.print_counter());
+        println!("RESULT: {:?}", response.print_counter());
     }
     // stop system and exit
     System::current().stop();
