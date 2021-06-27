f2af29acd0fe845303cab407496d0582dcbb911f
Alejandro Daneri <adaneri@fi.uba.ar>
2021-06-27 14:30:51 -0300

    borro move no necesario al create arbiter

--- src/actor_main.rs
+++ src/actor_main.rs
@@ -15,9 +15,9 @@ use counter::Counter;
 async fn main() {
     // start new actor
     let mut synonyms_actor = SynonymsActor::new();
-    let merriam_addr = SyncArbiter::start(1, move || MerriamWebsterActor::new());
-    let your_dict_addr = SyncArbiter::start(1, move || YourDictionaryActor::new());
-    let thes_addr = SyncArbiter::start(1, move || ThesaurusActor::new());
+    let merriam_addr = SyncArbiter::start(1, MerriamWebsterActor::new);
+    let your_dict_addr = SyncArbiter::start(1, YourDictionaryActor::new);
+    let thes_addr = SyncArbiter::start(1, ThesaurusActor::new);
 
     synonyms_actor.add_dictionary_actor(thes_addr.recipient());
     synonyms_actor.add_dictionary_actor(your_dict_addr.recipient());
