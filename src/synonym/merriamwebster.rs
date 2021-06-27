d9b5448dbdd3bb35aec1aa982bcc20a1d3006c59
romina <casal.romina@gmail.com>
2021-06-27 19:30:23 -0300

    Fix not found words at merriam

--- src/synonym/merriamwebster.rs
+++ src/synonym/merriamwebster.rs
@@ -19,26 +19,14 @@ impl Finder for MerriamWebster {
     }
 
     fn parse_body(&self, body: &str) -> Vec<String> {
-        let from = match body.find("thes-list syn-list") {
-            Some(v) => v,
-            None => {0},
-        };
-        let to = match body.find("thes-list rel-list") {
-            Some(v) => v,
-            None => {0},
-        };
+        let from = body.find("thes-list syn-list").unwrap_or(0);
+        let to = body.find("thes-list rel-list").unwrap_or(0);
         let body = &body[from..to];
         let synonyms = body
             .match_indices("<a class=\"\" href=\"/thesaurus/")
             .map(|matched| {
-                let synonym_beg = match body[matched.0..].find('>') {
-                    Some(v) => v + 1 + matched.0,
-                    None => {0},
-                };
-                let synonym_end = match body[synonym_beg..].find('<') {
-                    Some(v) => v + synonym_beg,
-                    None => {0}
-                };
+                let synonym_beg = body[matched.0..].find('>').unwrap_or(0) + 1 + matched.0;
+                let synonym_end = body[synonym_beg..].find('<').unwrap_or(0) + synonym_beg;
                 body[synonym_beg..synonym_end].to_owned()
             });
         synonyms.collect::<Vec<String>>()
